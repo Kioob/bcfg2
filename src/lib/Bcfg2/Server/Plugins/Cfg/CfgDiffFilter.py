37107ad79497a60eb3c19730889381789fd9091b
Olivier Bonvalet <ob@daevel.fr>
2019-05-22 20:49:55 +0200

    add a permanent cache on DiffFilter, to optimize BindStructures process
    It's a ugly hack, with hard-coded storage path, to reduce processing from
    ~15s to 150ms in our setup.
    
    Caching is based on file checksum, to avoid any update problem.

--- src/lib/Bcfg2/Server/Plugins/Cfg/CfgDiffFilter.py
+++ src/lib/Bcfg2/Server/Plugins/Cfg/CfgDiffFilter.py
@@ -1,5 +1,6 @@
 """ Handle .diff files, which apply diffs to plaintext files """
 
+import hashlib
 import os
 import tempfile
 from Bcfg2.Server.Plugin import PluginExecutionError
@@ -18,18 +19,45 @@ class CfgDiffFilter(CfgFilter):
     deprecated = True
 
     def modify_data(self, entry, metadata, data):
-        basehandle, basename = tempfile.mkstemp()
-        open(basename, 'w').write(data)
-        os.close(basehandle)
 
-        cmd = ["patch", "-u", "-f", basename]
+        # Use a cache for patch, based on file checksum
+
+        dirCache = "/var/cache/bcfg2-server/diffFilter/"
+
+        dataChecksum = hashlib.md5(data).hexdigest()
+        patchChecksum = hashlib.md5(self.data).hexdigest()
+
+        cacheName = dataChecksum +"-"+ patchChecksum
+
+        cacheFile = dirCache + cacheName
+
+        # If cache exists, then use it
+        if os.path.exists(cacheFile):
+            return open(cacheFile).read()
+
+        # We need to work on a temporary file
+        tmpFile = dirCache +"."+ cacheName +"."+ str(os.getpid())
+
+        # Create file to patch
+        fileHandle = open(tmpFile, "w")
+        fileHandle.write(data)
+        fileHandle.close()
+
+        # Path the file
+        cmd = ["patch", "-u", "-f", tmpFile]
         patch = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
         stderr = patch.communicate(input=self.data)[1]
         ret = patch.wait()
-        output = open(basename, 'r').read()
-        os.unlink(basename)
+
         if ret != 0:
+            os.unlink(tmpFile)
             raise PluginExecutionError("Error applying diff %s: %s" %
                                        (self.name, stderr))
+
+        # Keep result as cache
+        os.rename(tmpFile, cacheFile)
+
+        output = open(cacheFile).read()
+
         return output
     modify_data.__doc__ = CfgFilter.modify_data.__doc__
